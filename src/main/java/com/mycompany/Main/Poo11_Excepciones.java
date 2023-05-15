0c3f0b58ef20dcf9deb9238d7d7aefe3ddb4581a
evelyn-lg <lazaro.evelyn@outlook.com>
2023-05-15 00:03:20 -0600

    segundo commit

--- src/main/java/com/mycompany/Main/Poo11_Excepciones.java
+++ src/main/java/com/mycompany/Main/Poo11_Excepciones.java
@@ -5,5 +5,6 @@ public class Poo11_Excepciones {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("esto es solo para checar la conexion con guthub");
     }
 }
