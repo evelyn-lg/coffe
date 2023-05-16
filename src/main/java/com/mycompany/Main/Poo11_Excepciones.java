1972a7c06ba94cfb5a67927c7e2ca550c8a7728e
evelyn-lg <lazaro.evelyn@outlook.com>
2023-05-15 20:23:06 -0600

    otro comentario

--- src/main/java/com/mycompany/Main/Poo11_Excepciones.java
+++ src/main/java/com/mycompany/Main/Poo11_Excepciones.java
@@ -9,4 +9,8 @@ public class Poo11_Excepciones {
         System.out.println("Hello World!");
         System.out.println("esto es solo para checar la conexion con guthub");
     }
+
+/*
+    Hay más información sobre excepciones en el documento de Intro Poo
+    */
 }
